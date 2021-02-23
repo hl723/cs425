388dea21066f4ed16748bf33a4aeb5bc3dc4b87c
Hao Li <li18hao@gmail.com>
2021-02-22 19:29:55 -0500

    attached report for lab1

--- lab1/p6/capitalize_syscall.c
+++ lab1/p6/capitalize_syscall.c
@@ -4,6 +4,9 @@
 SYSCALL_DEFINE2(capitalize_syscall, char __user *, buffer, int, len) 
 {
     int i = 0;
+
+    if (buffer == NULL)
+        return 0;
     
     printk(KERN_INFO "%s\n", buffer);
     
